a152c362056d6200b6bb90ffe6f10322c58929bc
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-08-05 16:24:27 -0400

    Be explicit in accepting only html requests for the root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     # Your application routes go here
-    root "pages#home"
+    root "pages#home", format: "html"
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
