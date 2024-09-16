63e8411ae59bc8de9246db756ebfddaa19654d6b
Zachary Rollyson <zachary.rollyson@gsa.gov>
2024-09-16 15:04:21 -0400

    Add no-sandbox to pa11yci config
    See: https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-on-gitlabci

--- pa11yci.js
+++ pa11yci.js
@@ -2,7 +2,8 @@ let defaults = require("./pa11y.js");
 
 // set chrome path for github actions
 defaults.defaults.chromeLaunchConfig = {
-  "executablePath": "/usr/bin/google-chrome"
+	executablePath: "/usr/bin/google-chrome",
+	args: ["--no-sandbox", "--disable-setuid-sandbox"],
 };
 
 module.exports = defaults;
