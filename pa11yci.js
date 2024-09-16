57297ab2e25686b365ce1c83d5434f8cfb6d9af9
Zachary Rollyson <zachary.rollyson@gsa.gov>
2024-09-16 15:35:54 -0400

    change chrome executable path

--- pa11yci.js
+++ pa11yci.js
@@ -2,7 +2,8 @@ let defaults = require("./pa11y.js");
 
 // set chrome path for github actions
 defaults.defaults.chromeLaunchConfig = {
-	executablePath: "/usr/bin/google-chrome",
+	executablePath:
+		"/root/.cache/puppeteer/chrome/linux-128.0.6613.137/chrome-linux64/chrome",
 	args: ["--no-sandbox", "--disable-setuid-sandbox"],
 };
 
