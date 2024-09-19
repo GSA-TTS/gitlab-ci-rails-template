e48fb51e07d47a9f4fc076b1347c25491a7acb45
Zachary Rollyson <zachary.rollyson@gsa.gov>
2024-09-19 12:51:50 -0400

    make pa11y can find chrome on its own

--- pa11yci.js
+++ pa11yci.js
@@ -2,8 +2,6 @@ let defaults = require("./pa11y.js");
 
 // set chrome path for github actions
 defaults.defaults.chromeLaunchConfig = {
-	executablePath:
-		"/root/.cache/puppeteer/chrome/linux-128.0.6613.137/chrome-linux64/chrome",
 	args: ["--no-sandbox", "--disable-setuid-sandbox"],
 };
 
